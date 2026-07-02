a751359c4bdec0e005d3780c54a09119ea9bb199
rifzky11 <rifzkynurman11@gmail.com>
2026-07-02 16:36:36 +0700

    update ticketbooth's raycast

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -8,18 +8,19 @@ public class PlayerInteraction : MonoBehaviour
     [Header("UI Reference (Untuk Boneka/Umum)")]
     [SerializeField] private GameObject interactionUI;    
 
-    [Header("Hover Tint Settings")]
+    [Header("Hover Tint Settings (Khusus Boneka)")]
     [SerializeField] private Color hoverTargetColor = Color.green; 
     [SerializeField] [Range(0f, 1f)] private float tintStrength = 0.25f;
 
+    [Header("Material Glow Loket (Tarik Material GlowSamar Ke Sini!)")]
+    [SerializeField] private Material loketGlowMaterial;
+
     private Renderer[] lastHoveredRenderers;
-    private MaterialPropertyBlock propBlock;
+    private Material[][] originalMaterialsMesh; 
     private TicketInteraction ticketScriptCache;
 
     private void Awake()
     {
-        propBlock = new MaterialPropertyBlock();
-        // Ambil cache script TicketInteraction yang ada di objek kamera ini sendiri
         ticketScriptCache = GetComponent<TicketInteraction>();
     }
 
@@ -37,7 +38,6 @@ public class PlayerInteraction : MonoBehaviour
                 interactable = hit.collider.GetComponentInParent<IInteractable>();
             }
 
-            // DETEKSI KHUSUS LOKET TIKET BERDASARKAN TAG OBJEK YANG DITATAP
             bool isTicketBooth = hit.collider.CompareTag("TicketBooth");
             if (isTicketBooth && ticketScriptCache != null)
             {
@@ -57,20 +57,19 @@ public class PlayerInteraction : MonoBehaviour
                 // Pengaturan UI Canvas
                 if (isTicketBooth)
                 {
-                    SembunyikanUI(); // Sembunyikan UI "Press E" milik boneka
-                    if (ticketScriptCache != null) ticketScriptCache.SetPromptActive(true); // Nyalakan UI bawaan tiketmu!
+                    SembunyikanUI(); 
+                    if (ticketScriptCache != null) ticketScriptCache.SetPromptActive(true); 
                 }
                 else if (hit.collider.GetComponent<PushObject>() != null || hit.collider.GetComponentInParent<PushObject>() != null)
                 {
-                    SembunyikanSemuaUI(); // Boneka dorong tidak pakai UI
+                    SembunyikanSemuaUI(); 
                 }
                 else
                 {
-                    if (interactionUI != null) interactionUI.SetActive(true); // Boneka biasa pakai UI "Press E"
+                    if (interactionUI != null) interactionUI.SetActive(true); 
                     if (ticketScriptCache != null) ticketScriptCache.SetPromptActive(false);
                 }
 
-                // --- LOGIKA EMAS: AMBIL SEMUA GAMBER RENDERER DARI OBJEK YANG DITATAP ---
                 Renderer[] currentRenderers = hit.collider.GetComponentsInChildren<Renderer>();
                 if (currentRenderers.Length == 0)
                 {
@@ -80,18 +79,52 @@ public class PlayerInteraction : MonoBehaviour
                 if (currentRenderers.Length > 0 && (lastHoveredRenderers == null || lastHoveredRenderers[0] != currentRenderers[0]))
                 {
                     ResetGlow(); 
+                    
                     lastHoveredRenderers = currentRenderers;
+                    originalMaterialsMesh = new Material[currentRenderers.Length][];
 
-                    Color blendedColor = Color.Lerp(Color.white, hoverTargetColor, tintStrength);
-
-                    foreach (Renderer ren in lastHoveredRenderers)
+                    // JIKA TARGET ADALAH LOKET TIKET
+                    if (isTicketBooth && loketGlowMaterial != null)
                     {
-                        if (ren != null)
+                        for (int i = 0; i < currentRenderers.Length; i++)
                         {
-                            ren.GetPropertyBlock(propBlock);
-                            propBlock.SetColor("_Color", blendedColor);
-                            propBlock.SetColor("_BaseColor", blendedColor); 
-                            ren.SetPropertyBlock(propBlock);
+                            if (currentRenderers[i] != null)
+                            {
+                                originalMaterialsMesh[i] = currentRenderers[i].sharedMaterials;
+
+                                // Tambahkan material GlowSamar kamu di atas material glTF bawaan loket
+                                Material[] fakeGlowMaterials = new Material[originalMaterialsMesh[i].Length + 1];
+                                for (int j = 0; j < originalMaterialsMesh[i].Length; j++)
+                                {
+                                    fakeGlowMaterials[j] = originalMaterialsMesh[i][j];
+                                }
+                                
+                                // Taruh material buatanmu di lapisan terluar
+                                fakeGlowMaterials[fakeGlowMaterials.Length - 1] = loketGlowMaterial;
+                                currentRenderers[i].materials = fakeGlowMaterials;
+                            }
+                        }
+                    }
+                    // JIKA TARGET ADALAH BONEKA
+                    else
+                    {
+                        for (int i = 0; i < currentRenderers.Length; i++)
+                        {
+                            if (currentRenderers[i] != null)
+                            {
+                                originalMaterialsMesh[i] = currentRenderers[i].sharedMaterials;
+                                
+                                Material[] dynamicMaterials = currentRenderers[i].materials;
+                                for (int j = 0; j < dynamicMaterials.Length; j++)
+                                {
+                                    if (dynamicMaterials[j].HasProperty("_Color"))
+                                    {
+                                        Color origColor = dynamicMaterials[j].color;
+                                        dynamicMaterials[j].color = Color.Lerp(origColor, hoverTargetColor, tintStrength);
+                                    }
+                                }
+                                currentRenderers[i].materials = dynamicMaterials;
+                            }
                         }
                     }
                 }
@@ -116,19 +149,17 @@ public class PlayerInteraction : MonoBehaviour
 
     private void ResetGlow()
     {
-        if (lastHoveredRenderers != null)
+        if (lastHoveredRenderers != null && originalMaterialsMesh != null)
         {
-            foreach (Renderer ren in lastHoveredRenderers)
+            for (int i = 0; i < lastHoveredRenderers.Length; i++)
             {
-                if (ren != null)
+                if (lastHoveredRenderers[i] != null && i < originalMaterialsMesh.Length && originalMaterialsMesh[i] != null)
                 {
-                    ren.GetPropertyBlock(propBlock);
-                    propBlock.SetColor("_Color", Color.white);
-                    propBlock.SetColor("_BaseColor", Color.white);
-                    ren.SetPropertyBlock(propBlock);
+                    lastHoveredRenderers[i].materials = originalMaterialsMesh[i];
                 }
             }
             lastHoveredRenderers = null;
+            originalMaterialsMesh = null;
         }
     }
 
